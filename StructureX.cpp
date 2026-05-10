a0287f7c69330aa8b58402cc3d79ada9420f6865
pranavnarkhede45-ce <pranavnarkhede45@gmail.com>
2026-05-10 17:45:00 +0530

    Add program demonstrating the concept of structure in cpp

---
+++ StructureX.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+struct Demo                     
+{
+    int i;                      
+    float j;
+};
+
+int main()
+{
+    struct Demo dobj;          
+
+    dobj.i = 10;
+    dobj.j = 20.0f;
+
+    cout<<"Value of variable i :"<<dobj.i<<endl;
+    cout<<"Value of variable j :"<<dobj.j<<endl;
+
+    return 0;
+}
