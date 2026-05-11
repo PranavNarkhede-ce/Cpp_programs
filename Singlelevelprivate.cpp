d18db44fa6415581f981e0da18b8eac66510fad7
pranavnarkhede45-ce <pranavnarkhede45@gmail.com>
2026-05-11 16:44:26 +0530

    Practice : Single level inheritance in cpp

---
+++ Singlelevelprivate.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+using namespace std;
+
+class Base
+{
+    private:
+        int i , j;
+
+    public:
+        Base()
+        {
+            cout<<"inside base constructor \n";
+            i = 0;
+            j = 0;
+        }
+
+        ~Base()
+        {
+            cout<<"inside base destructor\n";
+        }
+
+        void fun()
+        {
+            cout<<"inside base fun\n";
+        }
+};
+
+class Derived : public Base
+{
+    public:
+        int x;
+
+    Derived()
+    {
+        cout<<"inside derived constructor\n";
+        x =0 ;
+    }
+
+    ~Derived()
+    {
+        cout<<"inside derived destructor\n";
+    }
+
+    void gun()
+    {
+        cout<<"inside gun of derived\n";
+    }
+
+};
+
+int main()
+{
+    cout<<"inside main\n";
+
+    Derived dobj;
+
+    cout<<"size of base class object is: "<<sizeof(Base)<<"\n";                     //8
+    cout<<"size of derived class object is: "<<sizeof(Derived)<<"\n";               //12
+
+   // cout<<dobj.i<<"\n";             // Error
+    // cout<<dobj.j<<"\n";            // Error 
+    cout<<dobj.x<<"\n";               //0
+
+    cout<<"end of main\n";
+
+    return 0;
+}
