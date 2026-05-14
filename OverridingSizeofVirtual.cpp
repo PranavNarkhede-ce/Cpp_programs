53cf6df62eeb3215ef51556d4ca1ce6c06c39a44
pranavnarkhede45-ce <pranavnarkhede45@gmail.com>
2026-05-14 20:51:36 +0530

    Add program of Virtual Function in cpp

---
+++ OverridingSizeofVirtual.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std ; 
+
+class Base
+{
+    public:
+        int i , j ;
+
+        void fun()
+        { cout<<"inside base fun\n";  }
+
+        virtual void gun()
+        { cout<<"inside base gun\n";  }
+
+        virtual void sun()
+        { cout<<"inside base sun\n";  }
+
+};          // 8 bytes
+
+class Derived : public Base
+{
+    public:
+        int x , y ;
+
+        void fun()                                              // redefination of fun 
+        { cout<<"inside derived fun\n";   }
+
+        void sun()                                              // redefination of sun 
+        { cout<<"inside derived sun\n";   }
+
+        virtual void run()                                              // defination  
+        { cout<<"inside derived run\n";   }
+
+};          // 16 bytes
+
+int main()
+{
+    cout<<sizeof(Base)<<"\n";               // 8
+
+    cout<<sizeof(Derived)<<"\n";            // 16
+
+    return 0 ;
+}
