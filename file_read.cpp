16cc262fc9ef71dbbd01a4e442c76309ec0ecd5d
pranavnarkhede45-ce <pranavnarkhede45@gmail.com>
2026-05-15 19:17:31 +0530

    Demonstrate file operations using system calls

---
+++ file_read.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<unistd.h>
+#include<fcntl.h>
+
+using namespace std;
+
+int main()
+{
+    int fd = 0 ;
+    int ret = 0 ;
+    char arr[20]  ;
+
+    fd = open("pranav.txt" , O_RDWR);
+
+    if(fd == -1)
+    {
+        cout<<"file not get opened \n";
+    }
+    else
+    {
+        cout<<"file get successfully open with fd :"<<fd<<"\n";
+
+        ret = read(fd, arr , 16);
+
+        cout<<ret <<"bytes gets written into the file \n";
+
+        cout<<arr<<"\n";
+
+    }
+    close(fd);
+
+    return 0;
+}
