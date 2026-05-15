a9906517fbdd28a8839990e39dead43218db73e3
pranavnarkhede45-ce <pranavnarkhede45@gmail.com>
2026-05-15 19:16:29 +0530

    Demonstrate file operations using system calls

---
+++ file_close.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<fcntl.h>
+#include<unistd.h>
+
+using namespace std;
+
+int main()
+{
+    int fd = 0 ;
+
+    fd = open("Pranav.txt",O_RDWR);
+
+    if(fd == -1)
+    {
+        cout<<"File is not opened \n";
+    }
+    else
+    {
+        cout<<"File gets successfully open " << fd <<"\n" ;
+
+    }
+
+    close(fd);                  // file gets closed 
+
+    return 0;
+}
